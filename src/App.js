ca7f655096c8b5915d8f9f989a3da2f4abeef11d
phyerim <aamm2222@hanmail.net>
2023-05-22 13:17:51 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { HashRouter, Routes ,Route,} from 'react-router-dom'
 import './App.css';
+import Mbti from './Mbti'
+import MbtiResult from './Mbti/MbtiResult'
+
 
 function App() {
+  
+  if (window.Kakao) {
+    const kakao = window.Kakao;
+    if (!kakao.isInitialized()) kakao.init('152cff34a176ba19c22f2f1b6260b315')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.StrictMode>
+        <HashRouter>
+            <Routes>
+              <Route path="/" element={<Mbti/>}/>
+              <Route path="/MbtiResult" element={<MbtiResult/>}/>
+            </Routes>
+        </HashRouter>
+      </React.StrictMode>
+ 
     </div>
   );
 }
---
+++ src/Mbti/MbtiResult.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom';
+import '../App.css'
+import './mbti.css'
+
+ 
+
+
+
+const MbtiResult =  (()=> {
+
+  const location = useLocation();
+  const mbtiContents = location?.state?.contents!==undefined?
+  location.state.contents:
+  {mbti:'',img:'',animal:'',animal2:'',contents:['']}
+
+  const navigate = useNavigate()
+ 
+  const setOneVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  setOneVh()
+
+  useEffect(()=>{
+    if(location?.state?.contents===undefined){
+      navigate('../mbti')
+    }
+    setOneVh();
+
+    function onResize(){ 
+        setOneVh();
+    }
+    window.addEventListener('resize',onResize);
+  },[])
+
+  return (
+    <div className='mbtiLayout'>
+          <div className='questionLayout'>
+              
+              <div className='mbtiTitle'>
+                <div>MBTI 테스트</div>
+                <div onClick={()=>navigate(-1)} style={{cursor:'pointer'}}>
+                    다시하기
+                </div>
+                
+              </div>
+              
+              <div className='questionList' style={{display:'flex'}}>
+                  
+                  
+                  <div className='questionItemLayout'>
+                    <div className='qProfileImg'>
+                      <div/> <div/>
+                    </div>
+
+                    <div className='chatListLayout'>
+                      <div className='qChatbox'>
+                        <div>◀</div> <div>당신의 MBTI는 '{mbtiContents.mbti}' 입니다.</div>
+                      </div>
+                      
+                      
+            
+                      <div style={{width:'auto',height:'auto',backgroundColor:'#fff',borderRadius:10,
+                           margin:'5px 5px 15px 5px',}}>
+                        <img src={mbtiContents.img} alt='' style={{width:130,height:130,marginTop:10}}/>
+                        <div style={{padding:'0px 15px 0px 15px'}}>
+                          {mbtiContents.animal}
+                        </div>
+                        <div style={{padding:'5px 5px 20px 5px',display:'flex',alignItems:'center',justifyContent:'center'}}>
+                          <div style={{fontWeight:700,fontSize:18}}>{mbtiContents.animal2}</div>
+                          <div style={{marginLeft:2}}>유형</div>
+                        </div>
+                      </div>
+ 
+
+                      <div className='qChatbox'>
+                        <div>◀</div> <div>{mbtiContents.mbti}는요</div>
+                      </div>
+
+                      {mbtiContents.contents.map((val,idx)=>
+                        <div className='qChatbox' key={idx}>
+                          <div>◀</div> <div>{val}</div>
+                        </div>
+                      )}
+                    </div>
+                  </div> 
+
+              
+
+              </div>
+          
+            </div>
+          
+     
+
+    
+    </div>
+  );
+})
+
+export default MbtiResult;
